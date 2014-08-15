e5e6ac4fbb62c1d35b76d58130c2f10007af35e5
maxalegre <max8abreu13@gmail.com>
2014-08-15 16:25:33 -0300

    filter-odd, nil-key

--- code-katas-1/src/code_katas_1/core.clj
+++ code-katas-1/src/code_katas_1/core.clj
@@ -4,12 +4,15 @@
   "Escribir una funcion que retorne solamente los numeros impares de
    una secuencia"
   [s]
+  (for [x s :when (odd? x)] x)
   )
 
 (defn nil-key
   "Escribir una funcion que dada una clave y un mapa, devuelva true, solamente si el mapa
    contiene una entrada con esa clave, y su valor es nil"
-  [k m])
+  [k m]
+  (if (nil? (k m)) true nil)
+  )
 
 (defn range
   "Escribir una funcion que cree una lista de enteros en un rango dado.
